d5d936f99c2ea4a0ebcdcb973f9881eed686741a
artiwnl <contatocarvalhoarthur@gmail.com>
2024-08-14 21:59:42 -0300

    Estudando sobre composição, objetos e enum

---
+++ ExercicioComposicao/Entities/Department.cs
@@ -0,0 +1,12 @@
+namespace ExercicioComposicao.Entities
+{
+    public class Department
+    {
+        public string Name { get; set; }
+        
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
